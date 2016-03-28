4023dac8809123b42f5337e887925ddb44bb234d
Yashas Roy <yashas.roy@gmail.com>
2016-03-28 16:47:16 -0500

    Completed q7

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -15,7 +15,12 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
+    count = 0
+    for word in words:
+        if len(word) >=2:
+            if word[0] == word[-1]:
+                count += 1
+    return count
 
 
 def front_x(words):
@@ -32,7 +37,14 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    begin_x = []
+    not_x = []
+    for i in words:
+        if i[0] == 'x':
+            begin_x.append(i)
+        else:
+            not_x.append(i)
+    return sorted(begin_x) + sorted(not_x)
 
 
 def sort_last(tuples):
@@ -49,7 +61,7 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+    return sorted(tuples, key = lambda x: x[-1])
 
 
 def remove_adjacent(nums):
@@ -68,9 +80,11 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
-
-
+    new_list = [nums[x] for x in range(len(nums)-1) if nums[x] != nums[x+1]]
+    if nums[-1] != new_list[-1]:
+        new_list.append(nums[-1])
+    return new_list
+    
 def linear_merge(list1, list2):
     """
     Given two lists sorted in increasing order, create and return a
@@ -85,4 +99,4 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    return sorted(list1 + list2)
