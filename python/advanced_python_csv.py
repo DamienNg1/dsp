e73bd160b6618df11a0d37cd7ce9038dba9afd65
Yashas Roy <yashas.roy@gmail.com>
2016-03-29 20:41:43 -0500

    Completed Part 2

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
 PLACE YOUR CODE HERE
+import csv
+
+f = open("/home/yashas/Documents/faculty.csv", "r")
+
+faculty = list(csv.reader(f))
+data = faculty[1:]
+
+emails = []
+for row in data:
+    emails.append(row[3].strip())
+    
+with open("dsp/python/emails.csv", "w") as f:
+    writer = csv.writer(f, lineterminator = '\n')
+    for email in emails:
+        writer.writerow([email])
